23a77fcf6814a14f2561c32476e82064d2f78598
hwangjongwon <ccccc1088@naver.com>
2021-08-14 22:24:15 +0900

    08-14 수정중

--- Web/app/main/index.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Blueprint, request, render_template, flash, redirect, url_for
-from flask import current_app as app
-
-main = Blueprint('main', __name__, url_prefix='/')
-
-@main.route('/main', methods=['GET'])
-def index():
-    return render_template('/main/index.html')
