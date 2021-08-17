897f6f66eea3089f676aa310611f3e3ae2657920
hwangjongwon <ccccc1088@naver.com>
2021-08-18 00:50:14 +0900

    .

--- Web/flaskapp/__init__.py
+++ Web/flaskapp/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, g, request, Response, make_response, session, render_te
 from flask import Markup, redirect, url_for
 from datetime import datetime, date, timedelta
 from werkzeug.utils import secure_filename
+import os
 app=Flask(__name__)
 app.debug = True
 #app.jinja_env.trim_blocks = True
@@ -27,8 +28,11 @@ def render_file():
 def upload_file():
     if request.method == 'POST':
         f=request.files['file'] 
-
-        f.save('./flaskapp/'+secure_filename(f.filename)) #저장할 경로 + 파일명
+        filename=f.filename
+        #dirname=f.filename[:4]
+        f.save('../yolov5/data/testing/'+secure_filename(f.filename)) #저장할 경로 + 파일명
+        cmd=("python ../yolov5/detect_test.py --source /data/testing/%s" %(filename))
+        os.system(cmd)
         return render_template('success.html')
 
 @app.route('/servicepage')
@@ -40,105 +44,6 @@ def main1():
     return render_template("mainpage.html")
 
 
-@app.route('/wc')
-def wc():
-    key=request.args.get('key')
-    val=request.args.get('val')
-    res=Response("SET COOKIE")
-    res.set_cookie(key, val)
-    session['Token']='123X'
-    return make_response(res)
-
-@app.route('/rc')
-def rc():
-    key=request.args.get('key') #token
-    val=request.cookies.get(key)
-    return "cookie['" + key + "] =" + val + "," + session.get('Token')
-
-@app.route('/delsess')
-def delsess():
-    if session.get('Token'):
-        del session['Token']
-    return "Session이 삭제되었습니다!"
-
-@app.route('/reqenv')
-def reqenv():
-    return ('REQUEST_METHOD: %(REQUEST_METHOD) s <br>'
-        'SCRIPT_NAME: %(SCRIPT_NAME) s <br>'
-        'PATH_INFO: %(PATH_INFO) s <br>'
-        'QUERY_STRING: %(QUERY_STRING) s <br>'
-        'SERVER_NAME: %(SERVER_NAME) s <br>'
-        'SERVER_PORT: %(SERVER_PORT) s <br>'
-        'SERVER_PROTOCOL: %(SERVER_PROTOCOL) s <br>'
-        'wsgi.version: %(wsgi.version) s <br>'
-        'wsgi.url_scheme: %(wsgi.url_scheme) s <br>'
-        'wsgi.input: %(wsgi.input) s <br>'
-        'wsgi.errors: %(wsgi.errors) s <br>'
-        'wsgi.multithread: %(wsgi.multithread) s <br>'
-        'wsgi.multiprocess: %(wsgi.multiprocess) s <br>'
-        'wsgi.run_once: %(wsgi.run_once) s') % request.environ
-
-
-
-# request 처리 용 함수
-def ymd(fmt):
-    def trans(date_str):
-        return datetime.strptime(date_str, fmt)
-    return trans
-
-
-@app.route('/dt')
-def dt():
-    datestr = request.values.get('date', date.today(), type=ymd('%Y-%m-%d'))
-    return "우리나라 시간 형식: " + str(datestr)
-
-
-
-#app.config['SERVER_NAME'] = 'localhost:5000'
-
-#@app.route("/sd")
-#def helloworld_local():
-#    return "Hello Local.com!"
-
-
-#subdomain으로 분리 가능
-#@app.route("/sd", subdomain="g")
-#def helloworld22():
-#    return "Hello G.Local.com!!!"
-
-
-@app.route('/rp')
-def rp():
-    q=request.args.getlist('q')
-    return "q=%s" % str(q)
-
-@app.route('/test_wsgi')
-def wsgi_test():
-    def application(environ, start_response):
-        body = 'The request method was %s' % environ['REQUEST_METHOD']
-        headers = [('Content-Type', 'text/plain'),
-                    ('Content-Length', str(len(body)))]
-                    
-        start_response('200 OK', headers)
-        return [body]
-    return make_response(application)
-
-
-
-@app.route('/res1')
-def res1():
-    custom_res = Response("Custom Response", 200, {'test': 'ttt'})
-    return make_response(custom_res)
-
- 
-#@app.before_request
-#def before_request():
-#    print("before_request!!!")
-#    g.str="한글"
-
-#@app.route("/gg")
-#def helloworld2():
-#    return "Hello Flask World!" + getattr(g, 'str', '111')
 
 
 
